8c524e9ac01332e8a45c09fb528cc30fc13dfd31
vikkmaster <virraan@live.se>
2023-04-26 13:08:49 +0200

    achievement+gitignore changed

--- lib/app_pages/progress_page/achievement_page.dart
+++ lib/app_pages/progress_page/achievement_page.dart
@@ -7,10 +7,145 @@ class AchievementPage extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text('Achievement page'),
+        title: const Text('Achievement page'),
       ),
-      body: Center(
-        child: Text('This is the achievement page'),
+      body: ListView(
+        // Add padding to the list view
+        padding: const EdgeInsets.all(16.0),
+        children: [
+          Card(
+            child: ListTile(
+              leading: Icon(
+                Icons.nature,
+                color: Colors.green[400],
+              ),
+              title: const Text(
+                'NatureLover',
+                style: TextStyle(
+                  fontSize: 16.0,
+                  fontWeight: FontWeight.bold,
+                ),
+              ),
+              subtitle: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  Text(
+                    'Level 0',
+                    style: TextStyle(
+                      fontSize: 14.0,
+                      fontWeight: FontWeight.bold,
+                      color: Colors.grey[600],
+                    ),
+                  ),
+                  const SizedBox(height: 8.0),
+                  Row(
+                    children: [
+                      Expanded(
+                        child: LinearProgressIndicator(
+                          value: 0.8,
+                          backgroundColor: Colors.grey[200]!,
+                          valueColor: AlwaysStoppedAnimation<Color>(
+                            Colors.green[400]!,
+                          ),
+                        ),
+                      ),
+                      const SizedBox(width: 8.0),
+                      Text('${(100-(0.8*100)).toStringAsFixed(0)}% left'),
+                    ],
+                  ),
+                ],
+              ),
+            ),
+          ),
+          Card(
+            child: ListTile(
+              leading: Icon(
+                Icons.flag,
+                color: Colors.blue[400],
+              ),
+              title: const Text(
+                'Flag achievement',
+                style: TextStyle(
+                  fontSize: 16.0,
+                  fontWeight: FontWeight.bold,
+                ),
+              ),
+              subtitle: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  Text(
+                    'Level 0',
+                    style: TextStyle(
+                      fontSize: 14.0,
+                      fontWeight: FontWeight.bold,
+                      color: Colors.grey[600],
+                    ),
+                  ),
+                  const SizedBox(height: 8.0),
+                  Row(
+                    children: [
+                      Expanded(
+                        child: LinearProgressIndicator(
+                          value: 0.8,
+                          backgroundColor: Colors.grey[200]!,
+                          valueColor: AlwaysStoppedAnimation<Color>(
+                            Colors.green[400]!,
+                          ),
+                        ),
+                      ),
+                      const SizedBox(width: 8.0),
+                      Text('${(100-(0.8*100)).toStringAsFixed(0)}% left'),
+                    ],
+                  ),
+                ],
+              ),
+            ),
+          ),
+          Card(
+            child: ListTile(
+              leading: Icon(
+                Icons.star,
+                color: Colors.deepOrange[400],
+              ),
+              title: const Text(
+                'Streak',
+                style: TextStyle(
+                  fontSize: 16.0,
+                  fontWeight: FontWeight.bold,
+                ),
+              ),
+              subtitle: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  Text(
+                    'Level 0',
+                    style: TextStyle(
+                      fontSize: 14.0,
+                      fontWeight: FontWeight.bold,
+                      color: Colors.grey[600],
+                    ),
+                  ),
+                  const SizedBox(height: 8.0),
+                  Row(
+                    children: [
+                      Expanded(
+                        child: LinearProgressIndicator(
+                          value: 0.8,
+                          backgroundColor: Colors.grey[200]!,
+                          valueColor: AlwaysStoppedAnimation<Color>(
+                            Colors.green[400]!,
+                          ),
+                        ),
+                      ),
+                      const SizedBox(width: 8.0),
+                      Text('${(100-(0.8*100)).toStringAsFixed(0)}% left'),
+                    ],
+                  ),
+                ],
+              ),
+            ),
+          ),
+        ],
       ),
     );
   }
