dfc760006bf6ef8a0157d05fab5b8a570c73f235
vikkmaster <virraan@live.se>
2023-04-27 12:26:28 +0200

    local_user: save and load to local phone

--- lib/database/local_user.dart
+++ lib/database/local_user.dart
@@ -1,3 +1,7 @@
+import 'package:shared_preferences/shared_preferences.dart';
+import 'package:urban_escape_application/app_pages/progress_page/goal_storage.dart';
+
+
 class LocalUser {
   final List<DateTime> _streakDates = []; // a list to store the dates of the user's streaks
 
@@ -5,7 +9,6 @@ class LocalUser {
   void addStreakDate(DateTime date) {
     _streakDates.add(date);
   }
-
   // a method to get the user's current streak
   int getCurrentStreak() {
     int streak = 0;
@@ -16,13 +19,28 @@ class LocalUser {
     return streak;
   }
 
+  Future<bool> goalAdded() async {
+    GoalStorage goalStorage = GoalStorage();
+    if (await goalStorage.readGoal() == 0){
+      return false;
+    }
+    return true;
+  }
+
+
+
   // a method to save the user's streak data to local storage
   Future<void> saveData() async {
-    // use a local storage solution like shared_preferences to save the user's streak data
+    SharedPreferences prefs = await SharedPreferences.getInstance();
+    List<String> dates = _streakDates.map((date) => date.toIso8601String()).toList();
+    await prefs.setStringList('streak_dates', dates);
   }
 
   // a method to load the user's streak data from local storage
   Future<void> loadData() async {
-    // use a local storage solution like shared_preferences to load the user's streak data
+    SharedPreferences prefs = await SharedPreferences.getInstance();
+    List<String> dates = prefs.getStringList('streak_dates') ?? [];
+    _streakDates.clear();
+    _streakDates.addAll(dates.map((date) => DateTime.parse(date)));
   }
 }
