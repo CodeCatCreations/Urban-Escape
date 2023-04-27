50bd174fe91f57cf668a7ad2997e8a3321f0c0a3
Oliver <oliver.gurcilov1nilsson@gmail.com>
2023-04-27 16:16:29 +0200

    AchievementPage: Implemented Pop-ups with description when clicking the achievements

--- lib/app_pages/progress_page/achievement_page.dart
+++ lib/app_pages/progress_page/achievement_page.dart
@@ -14,112 +14,117 @@ class AchievementPage extends StatelessWidget {
         // Add padding to the list view
         padding: const EdgeInsets.all(16.0),
         children: [
-          Card(
-            child: ListTile(
-              leading: Column(
-                mainAxisAlignment: MainAxisAlignment.center,
-                children: [
-                  Icon(
-                    Icons.nature,
-                    color: Colors.green[400],
-                  ),
-                  const SizedBox(height: 4.0),
-                  Text(
-                    'Level 111',
-                    style: TextStyle(
-                      fontSize: 14.0,
-                      fontWeight: FontWeight.bold,
-                      color: Colors.grey[600],
+          GestureDetector(
+            onTap: () => _showAchievementDetails(context, 'NatureLover', 'Description of NatureLover achievement'),
+            child: Card(
+              child: ListTile(
+                leading: Column(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    Icon(
+                      Icons.nature,
+                      color: Colors.green[400],
                     ),
-                  ),
-                ],
-              ),
-              title: const Text(
-                'NatureLover',
-                style: TextStyle(
-                  fontSize: 16.0,
-                  fontWeight: FontWeight.bold,
+                    const SizedBox(height: 4.0),
+                    Text(
+                      'Level 1',
+                      style: TextStyle(
+                        fontSize: 14.0,
+                        fontWeight: FontWeight.bold,
+                        color: Colors.grey[600],
+                      ),
+                    ),
+                  ],
                 ),
-              ),
-              subtitle: Column(
-                crossAxisAlignment: CrossAxisAlignment.start,
-                children: [
-                  const SizedBox(height: 8.0),
-                  Row(
-                    children: [
-                      Expanded(
-                        child: LinearProgressIndicator(
-                          // TODO: Get value from local_user
-                          value: 0.8,
-                          backgroundColor: Colors.grey[200]!,
-                          valueColor: AlwaysStoppedAnimation<Color>(
-                            Colors.green[400]!,
+                title: const Text(
+                  'Nature-Lover',
+                  style: TextStyle(
+                    fontSize: 16.0,
+                    fontWeight: FontWeight.bold,
+                  ),
+                ),
+                subtitle: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    const SizedBox(height: 8.0),
+                    Row(
+                      children: [
+                        Expanded(
+                          child: LinearProgressIndicator(
+                            // TODO: Get value from local_user
+                            value: 0.8,
+                            backgroundColor: Colors.grey[200]!,
+                            valueColor: AlwaysStoppedAnimation<Color>(
+                              Colors.green[400]!,
+                            ),
                           ),
                         ),
-                      ),
-                      const SizedBox(width: 8.0),
-                      Text('${(100-(0.8*100)).toStringAsFixed(0)}% left'),
-                    ],
-                  ),
-                ],
+                        const SizedBox(width: 8.0),
+                        Text('${(100-(0.8*100)).toStringAsFixed(0)}% left'),
+                      ],
+                    ),
+                  ],
+                ),
               ),
             ),
           ),
-
-          Card(
-            child: ListTile(
-              leading: Column(
-                mainAxisAlignment: MainAxisAlignment.center,
-                children: [
-                  Icon(
-                    Icons.flag,
-                    color: Colors.blue[400],
-                  ),
-                  const SizedBox(height: 4.0),
-                  Text(
-                    'Level 0',
-                    style: TextStyle(
-                      fontSize: 14.0,
-                      fontWeight: FontWeight.bold,
-                      color: Colors.grey[600],
+          GestureDetector(
+            onTap: () => _showAchievementDetails(context, 'Goal-oriented', 'Set a goal to reach level 1'),
+            child: Card(
+              child: ListTile(
+                leading: Column(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    Icon(
+                      Icons.flag,
+                      color: Colors.blue[400],
                     ),
-                  ),
-                ],
-              ),
-              title: const Text(
-                'Flag achievement',
-                style: TextStyle(
-                  fontSize: 16.0,
-                  fontWeight: FontWeight.bold,
+                    const SizedBox(height: 4.0),
+                    Text(
+                      'Level 0',
+                      style: TextStyle(
+                        fontSize: 14.0,
+                        fontWeight: FontWeight.bold,
+                        color: Colors.grey[600],
+                      ),
+                    ),
+                  ],
                 ),
-              ),
-              subtitle: Column(
-                crossAxisAlignment: CrossAxisAlignment.start,
-                children: [
-                  const SizedBox(height: 8.0),
-                  Row(
-                    children: [
-                      Expanded(
-                        child: LinearProgressIndicator(
-                          value: 0.8,
-                          backgroundColor: Colors.grey[200]!,
-                          valueColor: AlwaysStoppedAnimation<Color>(
-                            Colors.green[400]!,
+                title: const Text(
+                  'Setting Goals',
+                  style: TextStyle(
+                    fontSize: 16.0,
+                    fontWeight: FontWeight.bold,
+                  ),
+                ),
+                subtitle: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    const SizedBox(height: 8.0),
+                    Row(
+                      children: [
+                        Expanded(
+                          child: LinearProgressIndicator(
+                            value: 0.8,
+                            backgroundColor: Colors.grey[200]!,
+                            valueColor: AlwaysStoppedAnimation<Color>(
+                              Colors.green[400]!,
+                            ),
                           ),
                         ),
-                      ),
-                      const SizedBox(width: 8.0),
-                      Text('${(100-(0.8*100)).toStringAsFixed(0)}% left'),
-                    ],
-                  ),
-                ],
+                        const SizedBox(width: 8.0),
+                        Text('${(100-(0.8*100)).toStringAsFixed(0)}% left'),
+                      ],
+                    ),
+                  ],
+                ),
               ),
             ),
           ),
-
-
-          Card(
-            child: ListTile(
+          GestureDetector(
+          onTap: () => _showAchievementDetails(context, 'Streak', 'Maintain a five-day streak!'),
+          child: Card(
+          child: ListTile(
               leading: Column(
                 mainAxisAlignment: MainAxisAlignment.center,
                 children: [
@@ -168,9 +173,26 @@ class AchievementPage extends StatelessWidget {
               ),
             ),
           ),
-
+          ),
         ],
       ),
     );
   }
+  void _showAchievementDetails(BuildContext context, String title, String description) {
+    showDialog(
+      context: context,
+      builder: (BuildContext context) {
+        return AlertDialog(
+          title: Text(title),
+          content: Text(description),
+          actions: const <Widget>[
+          ],
+        );
+      },
+    );
+  }
+
+
+
+
 }
