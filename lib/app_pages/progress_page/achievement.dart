6605bed4baa0fafa42aefbdab18241f05c58e03b
Ticly <jonkanp@gmail.com>
2023-04-28 11:46:31 +0200

    added achievements class

---
+++ lib/app_pages/progress_page/achievement.dart
@@ -0,0 +1,90 @@
+
+
+import 'package:flutter/material.dart';
+
+class Achievement extends GestureDetector {
+  final String title;
+  final double percent = 0.8;
+  final String description;
+  final int level;
+  final Icon icon;
+
+  Achievement({
+    Key? key,
+    required this.title,
+    required this.description,
+    required this.level,
+    required this.icon,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return GestureDetector(
+            onTap: () => _showAchievementDetails(context, title, description),
+            child: Card(
+              child: ListTile(
+                leading: Column(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    Icon(
+                      Icons.nature,
+                      color: Colors.green[400],
+                    ),
+                    const SizedBox(height: 4.0),
+                    Text(
+                      level.toString(),
+                      style: TextStyle(
+                        fontSize: 14.0,
+                        fontWeight: FontWeight.bold,
+                        color: Colors.grey[600],
+                      ),
+                    ),
+                  ],
+                ),
+                title: Text(
+                  title,
+                  style: const TextStyle(
+                    fontSize: 16.0,
+                    fontWeight: FontWeight.bold,
+                  ),
+                ),
+                subtitle: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    const SizedBox(height: 8.0),
+                    Row(
+                      children: [
+                        Expanded(
+                          child: LinearProgressIndicator(
+                            // TODO: Get value from local_user
+                            value: percent,
+                            backgroundColor: Colors.grey[200]!,
+                            valueColor: AlwaysStoppedAnimation<Color>(
+                              Colors.green[400]!,
+                            ),
+                          ),
+                        ),
+                        const SizedBox(width: 8.0),
+                        Text('${(100-(percent*100)).toStringAsFixed(0)}% left'),
+                      ],
+                    ),
+                  ],
+                ),
+              ),
+            ),
+          );
+  }
+  void _showAchievementDetails(BuildContext context, String title, String description) {
+      showDialog(
+        context: context,
+        builder: (BuildContext context) {
+          return AlertDialog(
+            title: Text(title),
+            content: Text(description),
+            actions: const <Widget>[
+            ],
+          );
+        },
+      );
+    }
+}
