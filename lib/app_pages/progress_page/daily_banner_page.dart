2ff9a3bd8c79f7b119545ec8a93bdc2a9de8fd95
Jameel <jasa6359@student.su.se>
2023-05-09 15:43:25 +0200

    "Changes to progress banner"

--- lib/app_pages/progress_page/daily_banner_page.dart
+++ lib/app_pages/progress_page/daily_banner_page.dart
@@ -1,14 +1,27 @@
 import 'package:flutter/material.dart';
 
 class ProgressBannerBar {
-  // Define a static method named show that takes in a BuildContext and a String message
-  static void show(BuildContext context, String message) {
-    // Create a SnackBar with the given message and background color
+  static void show(BuildContext context, String message, {Duration duration = const Duration(seconds: 10)}) {
     final snackBar = SnackBar(
-      content: Text(message),
-      backgroundColor: Colors.blue,
+      content: Text(
+        message,
+        style: const TextStyle(
+          color: Colors.white,
+          fontSize: 15.0,
+        ),
+      ),
+      backgroundColor: Colors.green.shade600,
       behavior: SnackBarBehavior.floating,
+      duration: duration,
+      elevation: 10,
+      shape: const RoundedRectangleBorder(
+        borderRadius: BorderRadius.only(
+          topLeft: Radius.circular(10),
+          topRight: Radius.circular(10),
+        ),
+      ),
     );
+    
     ScaffoldMessenger.of(context).showSnackBar(snackBar);
   }
 }
