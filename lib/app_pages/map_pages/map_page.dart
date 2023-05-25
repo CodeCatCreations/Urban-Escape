519448ad041473149ac51d0413568fce472f03f3
Jameel <jasa6359@student.su.se>
2023-05-25 17:48:27 +0200

    "Added GPS to Maps"

--- lib/app_pages/map_pages/map_page.dart
+++ lib/app_pages/map_pages/map_page.dart
@@ -4,6 +4,7 @@ import 'package:google_maps_flutter/google_maps_flutter.dart';
 import 'package:urban_escape_application/app_pages/progress_page/daily_banner_page.dart';
 import 'package:urban_escape_application/database/maria_sql.dart';
 import 'package:urban_escape_application/database//local_user.dart';
+import 'package:geolocator/geolocator.dart';
 
 class MapPage extends StatefulWidget {
   const MapPage({super.key});
@@ -167,6 +168,34 @@ class _MapPageState extends State<MapPage> {
 
     setState(() {});
     LocalUser().saveData();
+  }
+
+    Future<Position> _determinePosition() async {
+    bool serviceEnabled;
+    LocationPermission permission;
+
+    serviceEnabled = await Geolocator.isLocationServiceEnabled();
+    if (!serviceEnabled) {
+      return Future.error("Location services are disabled");
+    }
+
+    permission = await Geolocator.checkPermission();
+
+    if (permission == LocationPermission.denied) {
+      permission = await Geolocator.requestPermission();
+
+      if (permission == LocationPermission.denied) {
+        return Future.error("Location permission denied");
+      }
+    }
+
+    if (permission == LocationPermission.deniedForever) {
+      return Future.error("Location permissions are permanently denied");
+    }
+
+    Position position = await Geolocator.getCurrentPosition();
+
+    return position;
   }
 
   @override
